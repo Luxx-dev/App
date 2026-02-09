54a9b628b1626848ecabc54f07b13f7980762f33
Lux <abdulmuqsitalade@gmail.com>
2026-02-09 00:19:40 -0500

    Update Enemy thing.lua

--- Enemy thing.lua
+++ Enemy thing.lua
@@ -9,7 +9,7 @@ local EnemyFolder = ReplicatedStorage.Assets.EnemiesFolder
 --//Utilities
 local Enemies = require(script.Enemies)
 
-local EnemyController = {}
+local EnemyManager = {}
 
 --//Variables
 local Count : number = 0
@@ -64,34 +64,34 @@ local function CalculateEnemy()
 	end
 end
 
-local function CalculatePosition<X, Y, Z>(x: number, y: number, z: number) : Vector3
+local function CalculatePosition(x: number, y: number, z: number) : Vector3
 	local randx = math.random(-x, x)
 	local randz = math.random(-z, z)
 	
 	return Vector3.new(randx, y, randz)
 end
 
-function EnemyController.GetWaveStatus()
+function EnemyManager.GetWaveStatus()
 	if Count > 0 then
 		return "Wave in session"
 	end
 	return "Wave ended"
 end
 
-function EnemyController.Init()
+function EnemyManager.Init()
 	local EnemyCount = 15
-	EnemyController.StartWave(EnemyCount)
+	EnemyManager.StartWave(EnemyCount)
 end
 
-function EnemyController.StartWave(EnemyCount)
+function EnemyManager.StartWave(EnemyCount)
 	if Count > 0 then return end
 	
 	for i = 1, EnemyCount do
-		EnemyController.SpawnEnemy()
+		EnemyManager.SpawnEnemy()
 	end
 end
 
-function EnemyController.SpawnEnemy()
+function EnemyManager.SpawnEnemy()
 	local Enemy = CalculateEnemy()
 	
 	local EnemyIndex = Count + 1
@@ -110,6 +110,8 @@ function EnemyController.SpawnEnemy()
 	EnemyStats.Models[EnemyIndex] = EnemyFolder:WaitForChild(Enemy.Name):Clone()
 	
 	local model = EnemyStats.Models[EnemyIndex]
+	if not model then return end
+	
 	local humanoid = model:WaitForChild("Humanoid")
 	local primarypart = model.PrimaryPart or model:FindFirstChildWhichIsA("BasePart")
 	
@@ -120,27 +122,27 @@ function EnemyController.SpawnEnemy()
 	primarypart.Position = CalculatePosition(500, 50, 500)
 	
 	local EnemiesFolder = workspace.Enemies
-	if not EnemyFolder then
+	if not EnemiesFolder then
 		EnemiesFolder = Instance.new("Folder")
 		EnemiesFolder.Name = "Enemies"
 		EnemiesFolder.Parent = workspace
 	end
 	model.Parent = EnemiesFolder
 	
-	EnemyController.FindTarget(EnemyIndex)
+	EnemyManager.FindTarget(EnemyIndex)
 end
 
-local MoveCd = 0.2
-local NextMoveCd = 2
-local radius = 20
-function EnemyController.FindTarget(EnemyIndex: number)
+local MoveCd = 0.4
+local NextMoveCd = 8
+function EnemyManager.FindTarget(EnemyIndex: number)
 	local connection
 	connection = RunService.Heartbeat:Connect(function(dt)
 		EnemyStats.Connections[EnemyIndex] = connection
 		
-		if EnemyStats.LastMove[EnemyIndex] and os.clock() - EnemyStats.LastMove[EnemyIndex] < MoveCd then return end
+		if EnemyStats.LastMove[EnemyIndex] and os.clock() - EnemyStats.LastMove[EnemyIndex] < MoveCd then 
+			return 
+		end
 		EnemyStats.LastMove[EnemyIndex] = os.clock()
-		
 		
 		local model = EnemyStats.Models[EnemyIndex]
 		local primarypart: BasePart = model.PrimaryPart or model:FindFirstChildWhichIsA("BasePart")
@@ -150,47 +152,69 @@ function EnemyController.FindTarget(EnemyIndex: number)
 		params.FilterType = Enum.RaycastFilterType.Exclude
 		params.FilterDescendantsInstances = {model}
 		
+		if EnemyStats.Targets[EnemyIndex] and EnemyStats.Targets[EnemyIndex]:FindFirstChild("Humanoid") then
+			if EnemyStats.Targets[EnemyIndex].Humanoid.Health <= 0 then 
+				EnemyStats.Targets[EnemyIndex] = nil
+				return
+			end
+			
+			if (EnemyStats.Targets[EnemyIndex].HumanoidRootPart.Position - model:FindFirstChild("HumanoidRootPart").Position).Magnitude < 1.5 then
+				EnemyManager.Attack(EnemyIndex)
+			end
+			
+			local PositionToMoveTo = EnemyStats.Targets[EnemyIndex].PrimaryPart.Position or EnemyStats.Targets[EnemyIndex]:FindFirstChildWhichIsA("BasePart")
+			if PositionToMoveTo then
+				humanoid:MoveTo(PositionToMoveTo)
+				EnemyStats.NextMove[EnemyIndex] = 0
+				return
+			end
+		end
+		
 		local target = nil
 		target = workspace:GetPartBoundsInRadius(primarypart.Position, EnemyStats.Radius[EnemyIndex], params)
 		
-		for _, Part in target do
-			if Part.Parent:FindFirstChild("Humanoid") and game.Players:GetPlayerFromCharacter(Part.Parent) then
-				EnemyStats.Targets[EnemyIndex] = Part.Parent
-				if (EnemyStats.Targets[EnemyIndex].HumanoidRootPart.Position - model:FindFirstChild("HumanoidRootPart").Position).Magnitude < 1.5 then
-					EnemyController.Attack(EnemyIndex)
-				end
+		if target then
+			for _, Part in target do
+				if Part.Parent:FindFirstChild("Humanoid") and game.Players:GetPlayerFromCharacter(Part.Parent) then
+					EnemyStats.Targets[EnemyIndex] = Part.Parent
+					if (EnemyStats.Targets[EnemyIndex].HumanoidRootPart.Position - model:FindFirstChild("HumanoidRootPart").Position).Magnitude < 1.5 then
+						EnemyManager.Attack(EnemyIndex)
+					end
 				
-				if Part.Parent.PrimaryPart or Part.Parent:FindFirstChildWhichIsA("BasePart") then
-					humanoid:MoveTo(Part.Parent.PrimaryPart.Position or Part.Parent:FindFirstChildWhichIsA("BasePart").Position)
-					EnemyStats.NextMove[EnemyIndex] = 0
-				end
-				
-			else
-				EnemyStats.Targets[EnemyIndex] = nil
-				EnemyStats.NextMove[EnemyIndex] += dt
-				print(EnemyStats.NextMove[EnemyIndex])
-				if EnemyStats.NextMove[EnemyIndex] >= NextMoveCd then
-					EnemyStats.NextMove[EnemyIndex] = 0
-					local PositionToMove = CalculatePosition(20, 0, 20)
-					humanoid:MoveTo(primarypart.Position + PositionToMove)
+					if Part.Parent.PrimaryPart or Part.Parent:FindFirstChildWhichIsA("BasePart") then
+						humanoid:MoveTo(Part.Parent.PrimaryPart.Position or Part.Parent:FindFirstChildWhichIsA("BasePart").Position)
+						EnemyStats.NextMove[EnemyIndex] = 0
+					end
+				else
+					EnemyStats.Targets[EnemyIndex] = nil
+					EnemyStats.NextMove[EnemyIndex] += MoveCd
+					print(EnemyStats.NextMove[EnemyIndex])
+					
+					if EnemyStats.NextMove[EnemyIndex] >= NextMoveCd then
+						EnemyStats.NextMove[EnemyIndex] = 0
+						local PositionToMove = CalculatePosition(20, 0, 20)
+						humanoid:MoveTo(primarypart.Position + PositionToMove)
+					end
 				end
 			end
 		end
 	end)
 end
 
-function EnemyController.Attack(EnemyIndex: number)
+function EnemyManager.Attack(EnemyIndex: number)
 	if EnemyStats.LastAttack[EnemyIndex] and os.clock() - EnemyStats.LastAttack[EnemyIndex] < EnemyStats.AttackCooldowns[EnemyIndex] then return end
 	EnemyStats.LastAttack[EnemyIndex] = os.clock()
 	
 	local target = EnemyStats.Targets[EnemyIndex]
-	if not target then return end
+	if not target then 
+		return
+	end
 	
 	local targetHumanoid = target:WaitForChild("Humanoid")
 	targetHumanoid:TakeDamage(EnemyStats.Damages[EnemyIndex])
 end
 
-function EnemyController.Destroy(EnemyIndex: number)
+function EnemyManager.Destroy(EnemyIndex: number)
 	
 	if EnemyStats.Connections[EnemyIndex] then
 		EnemyStats.Connections[EnemyIndex]:Disconnect()
@@ -231,10 +255,10 @@ function EnemyController.Destroy(EnemyIndex: number)
 	EnemyStats.Connections[lastindex] = nil
 end
 
-function EnemyController.KillAllEnemies()
+function EnemyManager.KillAllEnemies()
 	for i = Count, 1, -1 do
-		EnemyController.Destroy(i)
+		EnemyManager.Destroy(i)
 	end
 end
 
-return EnemyController
+return EnemyManager
